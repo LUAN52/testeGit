f14ea59c6dd57ee4c4bad661416d6b51169b120a
LUAN <luansouza600@gmail.com>
2019-09-07 09:41:06 -0300

    tamo junto

---
+++ php/nu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <?php
+        echo '<p>fodasse<p>';
+
+        for ($num = 1; $num<=100; $num++){
+            echo  "<p class = 'pop' > $num </p>";
+        
+        }
+    ?>
+</body>
+</html>
